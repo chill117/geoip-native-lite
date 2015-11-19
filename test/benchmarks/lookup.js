9bae8628e9862a1b77cc9f9cacbe30e54ccc43c9
Charles Hill <chill@degreesofzero.com>
2015-11-19 20:44:27 +0100

    Removed minimum ops/second requirement from benchmark tests.

--- test/benchmarks/lookup.js
+++ test/benchmarks/lookup.js
@@ -14,45 +14,32 @@ describe('benchmark: lookup', function() {
 		GeoIpNativeLite.loadData({ ipv6: true }, done);
 	});
 
-	var minHz = {
-		ipv4: 500000,
-		ipv6: 250000
-	};
-
 	_.each(_.keys(ips), function(ipType) {
 
 		var ipsArray = _.keys(ips[ipType]);
 
-		describe(ipType, function() {
+		it(ipType, function(done) {
 
-			it('should run at least ' + minHz[ipType] + ' ops/second', function(done) {
+			this.timeout(15000);
 
-				this.timeout(15000);
+			var i = 0;
 
-				var i = 0;
-
-				var bench = new Benchmark(function() {
-					var ip = ipsArray[i++] || ipsArray[i = 0];
-					GeoIpNativeLite.lookup(ip);
-				});
-
-				bench.on('complete', function(result) {
-
-					if (result.target.error) {
-						return done(result.target.error);
-					}
-
-					console.log(result.target.toString());
-
-					if (!(result.target.hz >= minHz[ipType])) {
-						return done(new Error('Expected at least ' + minHz[ipType] + ' ops/second'))
-					}
-
-					done();
-				});
-
-				bench.run();
+			var bench = new Benchmark(function() {
+				var ip = ipsArray[i++] || ipsArray[i = 0];
+				GeoIpNativeLite.lookup(ip);
 			});
+
+			bench.on('complete', function(result) {
+
+				if (result.target.error) {
+					return done(result.target.error);
+				}
+
+				console.log(result.target.toString());
+				done();
+			});
+
+			bench.run();
 		});
 	});
 });
